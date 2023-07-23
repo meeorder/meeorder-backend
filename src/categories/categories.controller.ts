40c1c53ac9463f2e0124613cfe272c98cc7ae358
Npwskp <nopawit.sukolpuk@gmail.com>
2023-07-24 00:27:54 +0700

    Add getall get and update service

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, HttpStatus, Post } from "@nestjs/common";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CategoriesService } from "./categories.service";
 import { CreateCategoryDto } from "./dto/create-category.dto";
 
@@ -8,8 +8,22 @@ import { CreateCategoryDto } from "./dto/create-category.dto";
 export class CategoriesController{
     constructor(private readonly categoriesService: CategoriesService){}
 
+    @ApiResponse({
+        status: HttpStatus.CREATED,
+        description: 'Create new category',
+        type: () => CreateCategoryDto,
+      })
     @Post()
     createCategory(@Body() doc: CreateCategoryDto){
         return this.categoriesService.createCategory(doc.title,doc.description)
+    }
+
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'Get all categories',
+    })
+    @Get()
+    getAllCategories(){
+        return this.categoriesService.getAllCategories();
     }
 }
