e25f2cee2cd22881c705e4d263ee9c96c548b349
namirinz <nathmcfc31115@gmail.com>
2023-07-26 19:15:24 +0700

    Add dbName params for choosing database name of MonogoDB.

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@ export enum Config {
   PORT = 'PORT',
   MONGO_URI = 'MONGO_URI',
   NODE_ENV = 'NODE_ENV',
+  MONGO_DB_NAME = 'MONGO_DB_NAME',
 }
 
 export const configuration = () => {
@@ -10,5 +11,6 @@ export const configuration = () => {
     [Config.PORT]: +(process.env.PORT ?? DEFAULT_PORT),
     [Config.MONGO_URI]: process.env.MONGO_URI,
     [Config.NODE_ENV]: process.env.NODE_ENV,
+    [Config.MONGO_DB_NAME]: process.env.MONGO_DB_NAME,
   };
 };
