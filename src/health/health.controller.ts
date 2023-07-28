c98000d35acfad48ceaeb1fecbf821d0ee9b5a00
Sila Sonpee <hirasawa_au@yahoo.co.jp>
2023-07-28 12:13:01 +0700

    E2E/cucumber (#22)
    * Cucumber test
    
    * Test
    
    * Fix CI
    
    * Fix CI
    
    * Fix CI
    
    * Fix CI
    
    * Fix scripts
    
    * Fix CI
    
    * Add option network
    
    * Curl test
    
    * Docker compose
    
    * Fix compose
    
    * Waiting
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Try to change ip address
    
    * Fix naming wrong

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -18,4 +18,14 @@ export class HealthController {
     const doc = await this.healthService.createRecord();
     return new HealthResponseDto(doc);
   }
+
+  @ApiResponse({
+    type: String,
+    status: HttpStatus.OK,
+    description: 'Health status (Not connected to DB)',
+  })
+  @Get('/ping')
+  getPing(): string {
+    return 'pong';
+  }
 }
