7152d6a9d61ad49725a8a17e3e0a757d389d62ec
Praepech Tophaiboon <122012803+PraepechPrAe@users.noreply.github.com>
2023-07-29 21:24:06 +0700

    module: tables (#28)
    * module: tables
    
    * define res

---
+++ src/tables/tables.controller.ts
@@ -0,0 +1,31 @@
+import { TablesSchema } from '@/schema/tables.schema';
+import { TablesDto } from '@/tables/dto/tables.dto';
+import { TablesService } from '@/tables/tables.service';
+import { Body, Controller, Get, HttpStatus, Post } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@Controller('tables')
+@ApiTags('tables')
+export class TablesController {
+  constructor(private readonly tablesService: TablesService) {}
+
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Create table',
+  })
+  @Post()
+  createTable(@Body() tables: TablesDto) {
+    return this.tablesService.createTable(tables._id);
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Get tables',
+    type: () => TablesSchema,
+    isArray: true,
+  })
+  @Get()
+  getTables() {
+    return this.tablesService.getTables();
+  }
+}
