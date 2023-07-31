4cbcb7cb5c3e8aefe4d851f82fbdf0c1802e9a1a
Npwskp <nopawit.sukolpuk@gmail.com>
2023-07-31 13:50:15 +0700

    add validator in dto

--- src/addons/dto/addon.dto.ts
+++ src/addons/dto/addon.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateAddonDto {
   @ApiProperty()
+  @IsString()
   title: string;
 
   @ApiProperty()
+  @IsNumber()
   price: number;
 }
