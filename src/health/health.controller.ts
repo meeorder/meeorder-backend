4e6df73195ed12761f866882702959d9daa15567
Sila Sonpee <hirasawa_au@yahoo.co.jp>
2023-08-27 00:05:38 +0700

    Fix e2e test

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,24 +1,9 @@
 import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { HealthResponseDto } from './dto/health.response.dto';
-import { HealthService } from './health.service';
 
 @Controller('health')
 @ApiTags('health')
 export class HealthController {
-  constructor(private readonly healthService: HealthService) {}
-
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'Health status',
-    type: () => HealthResponseDto,
-  })
-  @Get()
-  async getHealth(): Promise<HealthResponseDto> {
-    const doc = await this.healthService.createRecord();
-    return new HealthResponseDto(doc);
-  }
-
   @ApiResponse({
     type: String,
     status: HttpStatus.OK,
