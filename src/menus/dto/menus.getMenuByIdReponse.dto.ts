32f1cf2831ca79312b7cfc76f8294171a73b78fd
npwskp <86043260+Npwskp@users.noreply.github.com>
2023-09-05 15:09:30 +0700

    fix dto and schema (#83)
    * fix dto and schema
    
    * add description
    
    * fix type in dto

--- src/menus/dto/menus.getMenuByIdReponse.dto.ts
+++ src/menus/dto/menus.getMenuByIdReponse.dto.ts
@@ -1,33 +1,43 @@
 import { AddonSchema } from '@/schema/addons.schema';
+import { CategorySchema } from '@/schema/categories.schema';
 import { ApiProperty } from '@nestjs/swagger';
 import { Types } from 'mongoose';
 export class GetMenuByIdResponseDto {
   @ApiProperty({ type: String, description: 'Menu ID' })
   _id: Types.ObjectId;
 
-  @ApiProperty()
+  @ApiProperty({ type: String, nullable: true, description: 'Menu Image' })
   image: string;
 
-  @ApiProperty()
+  @ApiProperty({ required: true, type: String, description: 'Menu Title' })
   title: string;
-
-  @ApiProperty()
-  description: string;
-
-  @ApiProperty()
-  price: number;
 
   @ApiProperty({
     type: String,
+    nullable: true,
+    description: 'Menu Description',
   })
-  category: Types.ObjectId;
+  description: string;
 
-  @ApiProperty({ type: () => AddonSchema, isArray: true })
+  @ApiProperty({ required: true, type: Number, description: 'Menu Price' })
+  price: number;
+
+  @ApiProperty({
+    type: CategorySchema,
+    description: 'Menu Category',
+  })
+  category: CategorySchema;
+
+  @ApiProperty({
+    type: AddonSchema,
+    isArray: true,
+    description: 'Menu Addons',
+  })
   addons: AddonSchema[];
 
-  @ApiProperty()
+  @ApiProperty({ type: Date, description: 'Menu Published Date' })
   published_at: Date;
 
-  @ApiProperty()
+  @ApiProperty({ type: Date, nullable: true, description: 'Menu Deleted Date' })
   deleted_at: Date;
 }
--- src/menus/dto/menus.togetallMenu.dto.ts
+++ src/menus/dto/menus.togetallMenu.dto.ts
@@ -1,26 +1,17 @@
 import { AddonSchema } from '@/schema/addons.schema';
-import { CategorySchema } from '@/schema/categories.schema';
 import { ApiProperty } from '@nestjs/swagger';
-import { modelOptions, Prop, Ref } from '@typegoose/typegoose';
 import { Types } from 'mongoose';
 
-@modelOptions({
-  schemaOptions: { collection: 'menus' },
-})
-export class MenuSchema {
-  @Prop({ auto: true })
+export class MenuDtoForAllMenu {
   @ApiProperty({ type: String, description: 'Menu ID' })
   _id: Types.ObjectId;
 
-  @Prop({ default: null })
   @ApiProperty({ type: String, nullable: true, description: 'Menu Image' })
   image: string;
 
-  @Prop({ required: true })
   @ApiProperty({ type: String, description: 'Menu Title' })
   title: string;
 
-  @Prop({ default: null })
   @ApiProperty({
     type: String,
     nullable: true,
@@ -28,23 +19,19 @@ export class MenuSchema {
   })
   description: string;
 
-  @Prop({ required: true })
   @ApiProperty({ type: Number, description: 'Menu Price' })
   price: number;
 
-  @Prop({ required: true, ref: () => CategorySchema })
-  @ApiProperty({ description: 'Menu Category' })
-  category: Ref<CategorySchema>;
+  @ApiProperty({
+    type: AddonSchema,
+    isArray: true,
+    description: 'Menu Addons',
+  })
+  addons: AddonSchema[];
 
-  @Prop({ required: true, ref: () => AddonSchema, default: [] })
-  @ApiProperty({ description: 'Menu Addons' })
-  addons: Ref<AddonSchema>[];
-
-  @Prop({ required: true, default: new Date() })
   @ApiProperty({ type: Date, description: 'Menu Published Date' })
   published_at: Date;
 
-  @Prop({ default: null })
   @ApiProperty({ type: Date, nullable: true, description: 'Menu Deleted Date' })
   deleted_at: Date;
 }
