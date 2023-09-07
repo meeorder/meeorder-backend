e87660e64452040b267655d744ae8edebc72cc4a
Phusit Somboonyingsuk, phusitsom <phusitsom@gmail.com>
2023-09-07 21:11:20 +0700

    Fix the goddamn coupon feature fuck you (#103)
    * coupon fields: image, discount, quota, used
    
    * coupon: is_useable -> redeemable
    
    * coupons: even more usable->redeemable
    
    * Throw exception if coupon is out of quota
    
    * Update test for coupon
    
    * fuck coupon test
    
    * AHH FUCK YOU CUCUMBWR
    
    * I am sorry I love you cumcumba ❤️
    
    * UHDHHHHH
    
    * Please fuck me in the ass tonight
    
    * coupons: used->redeemed

--- features/step-definitions/coupon.ts
+++ features/step-definitions/coupon.ts
@@ -20,8 +20,9 @@ export class SessionStepDefination {
     for (const coupon of coupons) {
       const doc = await this.couponModel.create({
         title: coupon.title,
-        price: +coupon.price,
+        discount: +coupon.discount,
         required_point: +coupon.required_point,
+        quota: +coupon.quota,
         _id: new Types.ObjectId(coupon._id),
         activated: coupon.activated ?? true,
       });
@@ -37,8 +38,9 @@ export class SessionStepDefination {
       '/coupons',
       {
         title: coupon.title,
-        price: +coupon.price,
+        discount: +coupon.discount,
         required_point: +coupon.required_point,
+        quota: +coupon.quota,
       },
     );
   }
@@ -77,14 +79,14 @@ export class SessionStepDefination {
     expect(this.workspace.response.data.length).toBe(size);
   }
 
-  @then('coupon response id {string} usable should be {string}')
-  couponResponseShouldBe(id: string, usable: string) {
-    const isUsable = usable === 'true';
+  @then('coupon response id {string} redeemable should be {string}')
+  couponResponseShouldBe(id: string, redeemable: string) {
+    const isRedeemable = redeemable === 'true';
     const data: CouponDto[] = this.workspace.response.data;
     expect(Array.isArray(data)).toBeTruthy();
     const coupon = data.find((coupon) => `${coupon._id}` === id);
     expect(coupon).toBeDefined();
-    expect(coupon.is_useable).toEqual(isUsable);
+    expect(coupon.redeemable).toEqual(isRedeemable);
   }
 
   @after()
