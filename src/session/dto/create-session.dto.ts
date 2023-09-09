67206ff6c334cf40807a6e037bf954622bcf66e2
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-09-09 19:59:33 +0700

    session: remove user from CreateSessionDto

--- src/session/dto/create-session.dto.ts
+++ src/session/dto/create-session.dto.ts
@@ -1,14 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsOptional, IsUUID } from 'class-validator';
 import { Types } from 'mongoose';
 
 export class CreateSessionDto {
-  @ApiProperty({ type: String, description: 'User ID', required: false })
-  @IsOptional()
-  @IsUUID()
-  user?: string;
-
   @ApiProperty({ type: String, description: 'Table ID', required: true })
   @Transform(({ value }) => (value ? new Types.ObjectId(value) : null))
   table: Types.ObjectId;
