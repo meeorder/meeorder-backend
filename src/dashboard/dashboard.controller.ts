4bb9a4f4e9d160b506fe471d917ca0612baa7f83
Npwskp <nopawit.sukolpuk@gmail.com>
2023-09-24 02:01:56 +0700

    add apibearerauth

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,11 +1,19 @@
 import { DashboardService } from '@/dashboard/dashboard.service';
 import { GetUserAmountDto } from '@/dashboard/dto/getAllUserAmount.dto';
+import { Role } from '@/decorator/roles.decorator';
 import { ParseMongoDatePipe } from '@/pipes/mongo-date.pipe';
-import { Controller, Get, HttpStatus, Param } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserRole } from '@/schema/users.schema';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller({ path: 'dashboard', version: '1' })
 @ApiTags('dashboard')
+@ApiBearerAuth()
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
@@ -18,6 +26,8 @@ export class DashboardController {
     summary: 'Get total registered users',
   })
   @Get(':date')
+  @HttpCode(HttpStatus.OK)
+  @Role(UserRole.Owner)
   async getDashboard(@Param('date') date: number) {
     const parseDate = new ParseMongoDatePipe();
     return await this.dashboardService.getAllUserAmount(
