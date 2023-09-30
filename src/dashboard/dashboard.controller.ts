5b2c913fee5d2d25c282d4181a15d99ccdba2e99
Nath Prachayakul <nathmcfc31115@gmail.com>
2023-09-30 20:36:00 +0700

    Add dashboard/netIncome endpoint (#162)

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,12 +1,21 @@
 import { DashboardService } from '@/dashboard/dashboard.service';
 import { GetUserAmountDto } from '@/dashboard/dto/getAllUserAmount.dto';
+import { GetNetIncomeDto } from '@/dashboard/dto/getNetIncom.dto';
 import { Role } from '@/decorator/roles.decorator';
 import { ParseMongoDatePipe } from '@/pipes/mongo-date.pipe';
 import { UserRole } from '@/schema/users.schema';
-import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Query,
+} from '@nestjs/common';
 import {
   ApiBearerAuth,
   ApiOperation,
+  ApiQuery,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
@@ -31,6 +40,39 @@ export class DashboardController {
   async getDashboard(@Param('date') date: number) {
     return await this.dashboardService.getAllUserAmount(
       new ParseMongoDatePipe().transform(date),
+    );
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: () => GetNetIncomeDto,
+    description: 'Net income & Discount in range date',
+  })
+  @ApiOperation({
+    summary: 'Get net income & discount in range date',
+  })
+  @Get('/incomes_report')
+  @HttpCode(HttpStatus.OK)
+  @Role(UserRole.Owner)
+  @ApiQuery({
+    name: 'from',
+    type: Number,
+    required: true,
+    description: 'Start Date (UnixTimeStamp)',
+  })
+  @ApiQuery({
+    name: 'end',
+    type: Number,
+    required: true,
+    description: 'End Date (UnixTimeStamp)',
+  })
+  async getIncomeReport(
+    @Query('from') date_from: number,
+    @Query('end') date_end: number,
+  ) {
+    return await this.dashboardService.getIncomeReport(
+      new ParseMongoDatePipe().transform(date_from),
+      new ParseMongoDatePipe().transform(date_end),
     );
   }
 }
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,4 +1,5 @@
 import { GetUserAmountDto } from '@/dashboard/dto/getAllUserAmount.dto';
+import { ReceiptSchema } from '@/schema/receipt.schema';
 import { UserSchema } from '@/schema/users.schema';
 import { UsersService } from '@/users/users.service';
 import { Injectable } from '@nestjs/common';
@@ -10,6 +11,8 @@ export class DashboardService {
   constructor(
     @InjectModel(UserSchema)
     private readonly userModel: ReturnModelType<typeof UserSchema>,
+    @InjectModel(ReceiptSchema)
+    private readonly receiptModel: ReturnModelType<typeof ReceiptSchema>,
     private readonly userService: UsersService,
   ) {}
 
@@ -34,6 +37,44 @@ export class DashboardService {
       total_user: total,
       old_user,
       new_user: total - old_user,
+    };
+  }
+
+  async getIncomeReport(date_from: Date, date_end: Date) {
+    console.log(date_from, date_end);
+    const data = await this.receiptModel.aggregate([
+      {
+        $match: {
+          created_at: { $gte: date_from, $lte: date_end },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalIncome: {
+            $sum: '$total_price',
+          },
+          totalDiscount: {
+            $sum: '$discount_price',
+          },
+        },
+      },
+    ]);
+
+    let netIncome = 0;
+    let totalIncome = 0;
+    let totalDiscount = 0;
+
+    if (data.length !== 0) {
+      netIncome = data[0].totalIncome - data[0].totalDiscount;
+      totalIncome = data[0].totalIncome;
+      totalDiscount = data[0].totalDiscount;
+    }
+
+    return {
+      netIncome,
+      totalIncome,
+      totalDiscount,
     };
   }
 }
