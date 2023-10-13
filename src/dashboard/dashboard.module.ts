bb560f32bb6b0722a18621045ba511435a66b3de
Nath Prachayakul <nathmcfc31115@hotmail.com>
2023-10-13 16:53:21 +0000

    Add coupon_report endpoint today and total

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,5 +1,6 @@
 import { DashboardController } from '@/dashboard/dashboard.controller';
 import { DashboardService } from '@/dashboard/dashboard.service';
+import { CouponSchema } from '@/schema/coupons.schema';
 import { ReceiptSchema } from '@/schema/receipt.schema';
 import { UserSchema } from '@/schema/users.schema';
 import { UsersModule } from '@/users/users.module';
@@ -8,7 +9,7 @@ import { TypegooseModule } from 'nest-typegoose';
 
 @Module({
   imports: [
-    TypegooseModule.forFeature([UserSchema, ReceiptSchema]),
+    TypegooseModule.forFeature([UserSchema, ReceiptSchema, CouponSchema]),
     UsersModule,
   ],
   providers: [DashboardService],
