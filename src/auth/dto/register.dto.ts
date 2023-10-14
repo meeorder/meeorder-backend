aade62b29768079ac376cd286bf75612b78c6585
VjumpKunG <vjumpkung@gmail.com>
2023-10-14 21:40:05 +0700

    change with register and update user too

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,10 +1,7 @@
+import { MaxUsernameLength } from '@/utils/max-username-check';
+import { MinUsernameLength } from '@/utils/min-username-check';
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsAlphanumeric,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsAlphanumeric, IsString, Validate } from 'class-validator';
 
 const maxUsernameLength = 32;
 const minUsernameLength = 4;
@@ -12,8 +9,12 @@ const minUsernameLength = 4;
 export class RegisterDto {
   @ApiProperty({ type: String, description: 'Username' })
   @IsString()
-  @MaxLength(maxUsernameLength)
-  @MinLength(minUsernameLength)
+  @Validate(MinUsernameLength, {
+    message: `ชื่อผู้ใช้ต้องมีความยาวมากกว่าหรือเท่ากับ ${minUsernameLength} ตัวอักษร`,
+  })
+  @Validate(MaxUsernameLength, {
+    message: `ชื่อผู้ใช้ต้องมีความยาวน้อยกว่าหรือเท่ากับ ${maxUsernameLength} ตัวอักษร`,
+  })
   @IsAlphanumeric()
   username: string;
 
--- src/users/dto/user.update.info.dto.ts
+++ src/users/dto/user.update.info.dto.ts
@@ -2,21 +2,26 @@ import {
   maxUsernameLength,
   minUsernameLength,
 } from '@/users/dto/user.create.dto';
+import { MaxUsernameLength } from '@/utils/max-username-check';
+import { MinUsernameLength } from '@/utils/min-username-check';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsAlphanumeric,
   IsOptional,
   IsString,
-  MaxLength,
-  MinLength,
+  Validate,
 } from 'class-validator';
 
 export class UpdateInfoDto {
   @ApiPropertyOptional({ type: String, description: 'New Username' })
   @IsString()
   @IsOptional()
-  @MaxLength(maxUsernameLength)
-  @MinLength(minUsernameLength)
+  @Validate(MinUsernameLength, {
+    message: `ชื่อผู้ใช้ต้องมีความยาวมากกว่าหรือเท่ากับ ${minUsernameLength} ตัวอักษร`,
+  })
+  @Validate(MaxUsernameLength, {
+    message: `ชื่อผู้ใช้ต้องมีความยาวน้อยกว่าหรือเท่ากับ ${maxUsernameLength} ตัวอักษร`,
+  })
   @IsAlphanumeric()
   newUsername?: string;
 
