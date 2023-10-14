995324f4be6be1bde9b550ea06812b44d3282dcb
Praepech Tophaiboon <122012803+PraepechPrAe@users.noreply.github.com>
2023-10-14 21:23:43 +0700

    Feature/dashboard/netincome chart (#175)
    * report all daily netincome for barchart
    
    * -create_chart_report_dto
    -get all daily/monthly/yearly income
    
    * grouped chart
    
    * -grouped data's Chart
    
    * Squashed commit of the following:
    
    commit 6da86567d7957ea7ff5c9daee97f0c58a8200c27
    Merge: 5b2c913 3dbadc2
    Author: Chanrich Pisitjing <vjumpkung@gmail.com>
    Date:   Mon Oct 2 13:35:59 2023 +0700
    
        Merge pull request #172 from meeorder/fix/docs_update_user_info
    
        fix docs in UpdateInfoDto
    
    commit 3dbadc2b29b56e3bd3946f55b39753b473362c0c
    Author: VjumpKunG <vjumpkung@gmail.com>
    Date:   Mon Oct 2 01:16:53 2023 +0700
    
        fix docs in UpdateInfoDto
    
    commit 5b2c913fee5d2d25c282d4181a15d99ccdba2e99
    Author: Nath Prachayakul <nathmcfc31115@gmail.com>
    Date:   Sat Sep 30 20:36:00 2023 +0700
    
        Add dashboard/netIncome endpoint (#162)
    
    commit ea568bf60e157d7b225cd6baa514efa1648c9a6c
    Merge: 740cc6d 2c325c2
    Author: Chanrich Pisitjing <vjumpkung@gmail.com>
    Date:   Sat Sep 30 19:59:27 2023 +0700
    
        Merge pull request #167 from meeorder/fix/change_username_must_be_unique
    
        New username must be unique
    
    commit 740cc6d570b0f2660a5ea81710f329f0e1ba1103
    Author: Phusit Somboonyingsuk <phusitsom@gmail.com>
    Date:   Sat Sep 30 19:19:30 2023 +0700
    
        Orders: Get all -> populate menu ingredients (#169)
    
    commit 0f35baec28c81c753b7cdb0ccd43b7a84c2b6e17
    Merge: e7c6ea5 753661f
    Author: karnse <88821340+karnse@users.noreply.github.com>
    Date:   Sat Sep 30 19:02:07 2023 +0700
    
        Merge pull request #168 from meeorder/fix/cancel_order_should_change_status_too
    
        change order status to CANCELLED when cancel order
    
    commit 753661ff3991775cae44b885e40a90e391092143
    Author: vjumpkung <vjumpkung@gmail.com>
    Date:   Sat Sep 30 18:59:44 2023 +0700
    
        change order status to CANCELLED when cancel order
    
    commit 2c325c20d04915a0429ab2709df9014828fbe4ab
    Author: vjumpkung <vjumpkung@gmail.com>
    Date:   Sat Sep 30 18:54:18 2023 +0700
    
        new username must be unique
    
    commit e7c6ea551cba29831d972a1aa3f296b2eab3455a
    Author: Sila Sonpee <hirasawa_au@yahoo.co.jp>
    Date:   Sat Sep 30 17:59:14 2023 +0700
    
        Fix/Order Get Fixed OneOf (#165)
    
        karn
    
    commit 299f537cc68d1117fa233eecbb500a44b02385ac
    Author: Sila Sonpee <hirasawa_au@yahoo.co.jp>
    Date:   Sat Sep 30 14:27:42 2023 +0700
    
        Fix/Return All coupon when session not found (#161)
    
    commit dab82ed00a67632f509099eb2688fc5fa0890e90
    Merge: 60b8c58 0fedff0
    Author: Chanrich Pisitjing <vjumpkung@gmail.com>
    Date:   Sat Sep 30 01:20:10 2023 +0700
    
        Merge pull request #163 from meeorder/fix/user_info_change/language
    
        change error msg into Thai language
    
    commit 0fedff03a33776a55c926b44d1ddceb0cfda5d1c
    Author: vjumpkung <vjumpkung@gmail.com>
    Date:   Sat Sep 30 01:18:27 2023 +0700
    
        All Thai
    
    commit 0aa0b229eada8d17d2f055605a270b0319f7cb9d
    Author: vjumpkung <vjumpkung@gmail.com>
    Date:   Sat Sep 30 01:16:00 2023 +0700
    
        change error msg into Thai language
    
    * resolve conflict
    
    * add @ApiQuery
    
    * try resolve conflict_1
    
    * edit ApiQuery

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,4 +1,8 @@
 import { DashboardService } from '@/dashboard/dashboard.service';
+import { ChartDataDailyDto } from '@/dashboard/dto/chartData.daily.dto';
+import { ChartDataMonthlyDto } from '@/dashboard/dto/chartData.monthly.dto';
+import { ChartDataYearlyDto } from '@/dashboard/dto/chartData.yearly.dto';
+import { ChartGroupResponseDto } from '@/dashboard/dto/chartGroup.dto';
 import { GetReceiptAmountDto } from '@/dashboard/dto/getAllReceiptAmount.dto';
 import { GetCouponReportTodayDto } from '@/dashboard/dto/getCouponReportToday.dto';
 import { GetCouponReportTotalDto } from '@/dashboard/dto/getCouponReportTotal.dto';
@@ -75,6 +79,109 @@ export class DashboardController {
       new ParseMongoDatePipe().transform(date_from),
       new ParseMongoDatePipe().transform(date_end),
     );
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: () => ChartDataDailyDto,
+    description: 'All daily net income',
+  })
+  @ApiOperation({
+    summary: 'Get all daily net income',
+  })
+  @Get('/net_income/chart_data/daily')
+  @HttpCode(HttpStatus.OK)
+  @Role(UserRole.Owner)
+  async getDailyNetIncome() {
+    return await this.dashboardService.getAllDailyNetIncome();
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: () => ChartDataMonthlyDto,
+    description: 'All monthly net income',
+  })
+  @ApiOperation({
+    summary: 'Get all monthly net income',
+  })
+  @Get('/net_income/chart_data/monthly')
+  @HttpCode(HttpStatus.OK)
+  @Role(UserRole.Owner)
+  async getMonthlyNetIncome() {
+    return await this.dashboardService.getAllMonthlyNetIncome();
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: () => ChartDataYearlyDto,
+    description: 'All yearly net income',
+  })
+  @ApiOperation({
+    summary: 'Get all yearly net income',
+  })
+  @Get('/net_income/chart_data/yearly')
+  @HttpCode(HttpStatus.OK)
+  @Role(UserRole.Owner)
+  async getYearlyNetIncome() {
+    return await this.dashboardService.getAllYearlyNetIncome();
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: () => ChartGroupResponseDto,
+    description:
+      'Report net income grouped by hour, day of week, month, quarter',
+  })
+  @ApiOperation({
+    summary:
+      'Get report net income grouped by hour, day of week, month, quarter',
+  })
+  @Get('/net_income/chart_group')
+  @HttpCode(HttpStatus.OK)
+  @Role(UserRole.Owner)
+  @ApiQuery({
+    name: 'startTime',
+    type: Number,
+    required: true,
+    description: 'Start Date (UnixTimeStamp in seconds)',
+  })
+  @ApiQuery({
+    name: 'endTime',
+    type: Number,
+    required: true,
+    description: 'End Date (UnixTimeStamp in seconds)',
+  })
+  async getAllGroupedNetIncome(
+    @Query('startTime') startTime: number,
+    @Query('endTime') endTime: number,
+  ) {
+    const allGroupedNetIncome = new ChartGroupResponseDto();
+
+    allGroupedNetIncome.hourly =
+      await this.dashboardService.getHourGroupedNetIncome(
+        new ParseMongoDatePipe().transform(startTime),
+        new ParseMongoDatePipe().transform(endTime),
+      );
+
+    allGroupedNetIncome.daysOfWeek =
+      await this.dashboardService.getDayGroupedNetIncome(
+        new ParseMongoDatePipe().transform(startTime),
+        new ParseMongoDatePipe().transform(endTime),
+      );
+
+    allGroupedNetIncome.monthly =
+      await this.dashboardService.getMonthGroupedNetIncome(
+        new ParseMongoDatePipe().transform(startTime),
+        new ParseMongoDatePipe().transform(endTime),
+      );
+
+    allGroupedNetIncome.quarterly =
+      await this.dashboardService.getQuarterGroupedNetIncome(
+        new ParseMongoDatePipe().transform(startTime),
+        new ParseMongoDatePipe().transform(endTime),
+      );
+
+    return allGroupedNetIncome;
   }
 
   @ApiOperation({
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,4 +1,11 @@
+import { ChartDataDailyDto } from '@/dashboard/dto/chartData.daily.dto';
+import { ChartDataMonthlyDto } from '@/dashboard/dto/chartData.monthly.dto';
+import { ChartDataYearlyDto } from '@/dashboard/dto/chartData.yearly.dto';
+import { DaysOfWeekSubDto } from '@/dashboard/dto/daysOfWeek.sub.dto';
 import { GetReceiptAmountDto } from '@/dashboard/dto/getAllReceiptAmount.dto';
+import { HourlySubDto } from '@/dashboard/dto/hourly.sub.dto';
+import { MonthlySubDto } from '@/dashboard/dto/monthly.sub.dto';
+import { QuarterlySubDto } from '@/dashboard/dto/quarterly.sub.dto';
 import { CouponSchema } from '@/schema/coupons.schema';
 import { ReceiptSchema } from '@/schema/receipt.schema';
 import { UserSchema } from '@/schema/users.schema';
@@ -102,6 +109,265 @@ export class DashboardService {
       totalIncome,
       totalDiscount,
     };
+  }
+
+  async getAllDailyNetIncome(): Promise<ChartDataDailyDto[]> {
+    const agg = await this.receiptModel.aggregate([
+      {
+        $group: {
+          _id: {
+            date: {
+              $dateToString: { format: '%Y-%m-%d', date: '$created_at' },
+            },
+          },
+          total_price: { $sum: '$total_price' },
+          discount_price: { $sum: '$discount_price' },
+        },
+      },
+    ]);
+    return agg.map((item) => {
+      return {
+        date: +new Date(item._id.date),
+        netIncome: item.total_price - item.discount_price,
+      };
+    });
+  }
+
+  async getAllMonthlyNetIncome(): Promise<ChartDataMonthlyDto[]> {
+    const agg = await this.receiptModel.aggregate([
+      {
+        $group: {
+          _id: {
+            month: {
+              $dateToString: { format: '%Y-%m', date: '$created_at' },
+            },
+          },
+          total_price: { $sum: '$total_price' },
+          discount_price: { $sum: '$discount_price' },
+        },
+      },
+    ]);
+    return agg.map((item) => {
+      return {
+        month: +new Date(item._id.month),
+        netIncome: item.total_price - item.discount_price,
+      };
+    });
+  }
+
+  async getAllYearlyNetIncome(): Promise<ChartDataYearlyDto[]> {
+    const agg = await this.receiptModel.aggregate([
+      {
+        $group: {
+          _id: {
+            year: {
+              $dateToString: { format: '%Y', date: '$created_at' },
+            },
+          },
+          total_price: { $sum: '$total_price' },
+          discount_price: { $sum: '$discount_price' },
+        },
+      },
+    ]);
+    return agg.map((item) => {
+      return {
+        year: +new Date(item._id.year),
+        netIncome: item.total_price - item.discount_price,
+      };
+    });
+  }
+
+  // All grouped net income
+  async getDayGroupedNetIncome(startTime: Date, endTime: Date) {
+    const days = { Sun: 1, Mon: 2, Tue: 3, Wed: 4, Thu: 5, Fri: 6, Sat: 7 };
+    const agg = await this.receiptModel.aggregate([
+      {
+        $match: {
+          created_at: {
+            $gte: startTime,
+            $lte: endTime,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            dayOfWeek: { $dayOfWeek: '$created_at' },
+          },
+          total_price: { $sum: '$total_price' },
+          discount_price: { $sum: '$discount_price' },
+        },
+      },
+      {
+        $project: {
+          total_price: 1,
+          discount_price: 1,
+          _id: 1,
+          dayOfWeek: {
+            $switch: {
+              branches: [
+                { case: { $eq: ['$_id.dayOfWeek', days.Sun] }, then: 'Sun' },
+                { case: { $eq: ['$_id.dayOfWeek', days.Mon] }, then: 'Mon' },
+                { case: { $eq: ['$_id.dayOfWeek', days.Tue] }, then: 'Tue' },
+                { case: { $eq: ['$_id.dayOfWeek', days.Wed] }, then: 'Wed' },
+                { case: { $eq: ['$_id.dayOfWeek', days.Thu] }, then: 'Thu' },
+                { case: { $eq: ['$_id.dayOfWeek', days.Fri] }, then: 'Fri' },
+                { case: { $eq: ['$_id.dayOfWeek', days.Sat] }, then: 'Sat' },
+              ],
+              default: 'Unknown',
+            },
+          },
+        },
+      },
+    ]);
+    const dto = new DaysOfWeekSubDto();
+    agg.forEach((item) => {
+      dto[item.dayOfWeek] = item.total_price - item.discount_price;
+    });
+    return dto;
+  }
+
+  async getHourGroupedNetIncome(startTime: Date, endTime: Date) {
+    const agg = await this.receiptModel.aggregate([
+      {
+        $match: {
+          created_at: {
+            $gte: startTime,
+            $lte: endTime,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            hour: { $hour: '$created_at' },
+          },
+          total_price: { $sum: '$total_price' },
+          discount_price: { $sum: '$discount_price' },
+        },
+      },
+      {
+        $project: {
+          total_price: 1,
+          discount_price: 1,
+          _id: 1,
+          hour: '$_id.hour',
+        },
+      },
+    ]);
+    const dto = new HourlySubDto();
+    agg.forEach((item) => {
+      dto[item.hour] = item.total_price - item.discount_price;
+    });
+    return dto;
+  }
+
+  async getMonthGroupedNetIncome(startTime: Date, endTime: Date) {
+    const month = {
+      Jan: 1,
+      Feb: 2,
+      Mar: 3,
+      Apr: 4,
+      May: 5,
+      Jun: 6,
+      Jul: 7,
+      Aug: 8,
+      Sep: 9,
+      Oct: 10,
+      Nov: 11,
+      Dec: 12,
+    };
+    const agg = await this.receiptModel.aggregate([
+      {
+        $match: {
+          created_at: {
+            $gte: startTime,
+            $lte: endTime,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            month: { $month: '$created_at' },
+          },
+          total_price: { $sum: '$total_price' },
+          discount_price: { $sum: '$discount_price' },
+        },
+      },
+      {
+        $project: {
+          total_price: 1,
+          discount_price: 1,
+          _id: 1,
+          month: {
+            $switch: {
+              branches: [
+                { case: { $eq: ['$_id.month', month.Jan] }, then: 'Jan' },
+                { case: { $eq: ['$_id.month', month.Feb] }, then: 'Feb' },
+                { case: { $eq: ['$_id.month', month.Mar] }, then: 'Mar' },
+                { case: { $eq: ['$_id.month', month.Apr] }, then: 'Apr' },
+                { case: { $eq: ['$_id.month', month.May] }, then: 'May' },
+                { case: { $eq: ['$_id.month', month.Jun] }, then: 'Jun' },
+                { case: { $eq: ['$_id.month', month.Jul] }, then: 'Jul' },
+                { case: { $eq: ['$_id.month', month.Aug] }, then: 'Aug' },
+                { case: { $eq: ['$_id.month', month.Sep] }, then: 'Sep' },
+                { case: { $eq: ['$_id.month', month.Oct] }, then: 'Oct' },
+                { case: { $eq: ['$_id.month', month.Nov] }, then: 'Nov' },
+                { case: { $eq: ['$_id.month', month.Dec] }, then: 'Dec' },
+              ],
+              default: 'Unknown',
+            },
+          },
+        },
+      },
+    ]);
+    const dto = new MonthlySubDto();
+    agg.forEach((item) => {
+      dto[item.month] = item.total_price - item.discount_price;
+    });
+    return dto;
+  }
+
+  async getQuarterGroupedNetIncome(startTime: Date, endTime: Date) {
+    const three = 3;
+    const agg = await this.receiptModel.aggregate([
+      {
+        $match: {
+          created_at: {
+            $gte: startTime,
+            $lte: endTime,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            quarter: {
+              $ceil: {
+                $divide: [{ $subtract: [{ $month: '$created_at' }, 1] }, three],
+              },
+            },
+          },
+          total_price: { $sum: '$total_price' },
+          discount_price: { $sum: '$discount_price' },
+        },
+      },
+      {
+        $project: {
+          total_price: 1,
+          discount_price: 1,
+          _id: 1,
+          quarter: '$_id.quarter',
+        },
+      },
+    ]);
+    const dto = new QuarterlySubDto();
+    agg.forEach((item) => {
+      dto['Q' + item.quarter.toString()] =
+        item.total_price - item.discount_price;
+    });
+    return dto;
   }
 
   async getCouponReportToday() {
